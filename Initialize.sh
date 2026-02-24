24dec5df0de52304073a02aab38939f1ebee7d44
howardnm <bueess@163.com>
2026-02-24 12:57:16 +0800

    init

--- Initialize.sh
+++ Initialize.sh
@@ -3,11 +3,19 @@
 python manage.py makemigrations
 python manage.py migrate
 
-# 2. 导入初始化数据
+# 2. 收集静态文件
+python manage.py collectstatic
+# nginx设置文件夹映射
+# location /static/ {
+#     alias /www/wwwroot/django_manage/staticfiles/;  # 静态文件绝对路径
+#     expires 30d;  # 缓存静态文件
+# }
+
+# 3. 导入初始化数据
 python ./init/init_configs.py
 python ./init/init_materials_data.py
 python ./init/init_oem_data.py
 python ./init/init_raw_materials_data.py
 
-# 3. 创建超级管理员（请根据终端指示，填入对应的信息）
+# 4. 创建超级管理员（请根据终端指示，填入对应的信息）
 python manage.py createsuperuser
