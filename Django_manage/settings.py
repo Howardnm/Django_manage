d4eed87cdba8a12ff41cf752694e80172eb1f1cc
howardnm <bueess@163.com>
2026-07-10 11:56:42 +0800

    修改setting的数据库硬编码问题

--- Django_manage/settings.py
+++ Django_manage/settings.py
@@ -126,12 +126,12 @@ WSGI_APPLICATION = 'Django_manage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_manage',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_NAME', 'django_manage'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '123456'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
