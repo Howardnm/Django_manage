f157fff3ae5e03a6651b4426cc6d791e274a29f0
howardnm <bueess@163.com>
2026-09-10 15:23:26 +0800

    修改filter组件

---
+++ common_utils/constants.py
@@ -0,0 +1,8 @@
+"""跨 app 共享常量"""
+
+# ISO/ASTM 标准切换 Tab 定义
+# value: URL 参数值；label: Tab 完整文案；short: 按钮样式的紧凑文案
+STD_TABS = [
+    {'value': 'ISO', 'label': 'ISO / 国标', 'short': 'ISO'},
+    {'value': 'ASTM', 'label': 'ASTM (美标)', 'short': 'ASTM'},
+]
